c79988f4ec4167928aac6d098274566f0620877d
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-16 16:43:27 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 // Copyright (c) 2022 Kaitlyn Ip All rights reserved
 //
 // Created by: Kaitlyn Ip
-// Created on: Mar 2022
+// Created on: May 2022
 // This file contains xxx
+"use strict"
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-x-yy-JS/sw.js", {
+    scope: "/ICS2O-Unit-x-yy-JS/",
+  });
+}
 
 /**
- * This function xxx.
+ * Input
  */
+function myButtonClicked() {
+
