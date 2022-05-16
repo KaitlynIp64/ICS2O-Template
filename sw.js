f4c42394203e701e1fce2ec335f1dca9e2780988
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-16 16:47:25 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+var GHPATH = "/ICS2O-Unit-x-yy-JS/"
+var APP_PREFIX = "gppwa_"
+var VERSION = "version_001"
+var URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/css/style.css`,
+  `${GHPATH}/android-chrome-512x512.png`,
+  `${GHPATH}/js/script.js`,
+]
+
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener("fetch", function (e) {
+  console.log("Fetch request : " + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log("Responding with cache : " + e.request.url)
+        return request
+      } else {
+        console.log("File is not cached, fetching : " + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME)
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            console.log("Deleting cache : " + keyList[i])
+            return caches.delete(keyList[i])
+          }
+        })
+      )
+    })
+  )
+})
